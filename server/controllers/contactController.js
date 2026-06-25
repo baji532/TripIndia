6da0f6e406ca3e7e7d9b03975b7b0086229f824d
sk Baji <bajisk045@gmail.com>
2026-06-25 10:27:18 +0530

    Switch to Brevo SMTP

--- server/controllers/contactController.js
+++ server/controllers/contactController.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+    user: process.env.BREVO_USER,
+    pass: process.env.BREVO_PASS
   }
 });
 
